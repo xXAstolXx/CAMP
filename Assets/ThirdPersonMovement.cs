b964d7f2ae681b6ef399b1fbe1878ee710d1d5fb
David Teinert <DavidTeinert344@t-online.de>
2023-04-16 12:08:57 +0200

    some changes

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public CharacterController controller;
+
+   public float speed = 6f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3 (horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f) 
+        { 
+            controller.Move (direction * speed * Time.deltaTime);
+        }
     }
 }
