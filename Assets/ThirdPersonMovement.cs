9950f625f76a8644430531c1820e1560aa3e8543
Astol <DavidTeinert344@t-online.de>
2023-05-08 13:56:56 +0200

    some tries

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
@@ -17,9 +18,26 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField]
     float turnSmoothVelocity;
 
+    public float groundDrag;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
     // Update is called once per frame
     void Update()
     {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
@@ -34,5 +52,26 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move (moveDir.normalized * speed * Time.deltaTime);
         }
+        //when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+    private void Jump()
+    {
+        //reset y velocity 
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        controller.Move(transform.up * jumpForce);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
